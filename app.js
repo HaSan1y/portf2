d465d2288b80f99e787b2306d4ac52493abf0572
Hasan Yildirim <endplay_xp@hotmail.com>
2024-04-08 15:11:50 +0200

    adsens

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const allsections = document.querySelector(".main-content");
 
 function PageTransitions() {
   for (let i = 0; i < sectBtn.length; i++) {
-
     sectBtn[i].addEventListener("click", function () {
       let currentBtn = document.querySelectorAll(".active-btn");
       currentBtn[0].className = currentBtn[0].className.replace(
         "active-btn",
-        "control"
-      ); //control instead of ''empty..
+        "control"//
+      );
       this.className += " active-btn"; 
     });
   }
@@ -19,15 +18,12 @@ function PageTransitions() {
   allsections.addEventListener("click", (eu) => {
     const ido = eu.target.dataset.id; 
     if (ido) {
-      //if clicket remove readd each active and also remove readd active on section html element
       sectBtn.forEach((btn) => {
         btn.classList.remove("active"); 
       });
       eu.target.classList.add("active"); 
 
-
       sections.forEach((section) => {
-
         section.classList.remove("active");
       });
       const element = document.getElementById(ido);
