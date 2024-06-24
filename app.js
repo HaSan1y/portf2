f6d010ba9a9dad84e08b312ceddcd1b4bb6ec22d
Hasan Yildirim <endplay_xp@hotmail.com>
2024-06-24 16:13:14 +0200

    css

--- app.js
+++ app.js
@@ -4,39 +4,37 @@ const sectBtn = document.querySelectorAll(".control");
 const allsections = document.querySelector(".main-content");
 
 function PageTransitions() {
-  for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener("click", function () {
-      let currentBtn = document.querySelectorAll(".active-btn");
-      currentBtn[0].className = currentBtn[0].className.replace(
-        "active-btn",
-        "control"//
-      );
-      this.className += " active-btn"; 
-    });
-  }
+	for (let i = 0; i < sectBtn.length; i++) {
+		sectBtn[i].addEventListener("click", function () {
+			let currentBtn = document.querySelectorAll(".active-btn");
+			currentBtn[0].className = currentBtn[0].className.replace(
+				"active-btn",
+				"control", //
+			);
+			this.className += " active-btn";
+		});
+	}
 
-  allsections.addEventListener("click", (eu) => {
-    const ido = eu.target.dataset.id; 
-    if (ido) {
-      sectBtn.forEach((btn) => {
-        btn.classList.remove("active"); 
-      });
-      eu.target.classList.add("active"); 
+	allsections.addEventListener("click", (eu) => {
+		const ido = eu.target.dataset.id;
+		if (ido) {
+			sectBtn.forEach((btn) => {
+				btn.classList.remove("active");
+			});
+			eu.target.classList.add("active");
 
-      sections.forEach((section) => {
-        section.classList.remove("active");
-      });
-      const element = document.getElementById(ido);
-      element.classList.add("active"); 
-    }
+			sections.forEach((section) => {
+				section.classList.remove("active");
+			});
+			const element = document.getElementById(ido);
+			element.classList.add("active");
+		}
+	});
 
-  });
-
-  const themeBtn = document.querySelector(".theme-btn");
-  themeBtn.addEventListener("click", () => {
-    let element = document.body;
-    element.classList.toggle("light-mode");
-  });
+	const themeBtn = document.querySelector(".theme-btn");
+	themeBtn.addEventListener("click", () => {
+		document.body.classList.toggle("light-mode");
+	});
 }
 
 PageTransitions();
